f48bd5bd1e72835a584c115fa4c7e801409b5056
jgonzalez <jgonzalez@106-07.info2>
2023-01-18 13:54:40 +0100

    18/01/2023 Ejercicio06 completo

--- Tema5/src/ejerciciosT5CopiaInsercion/Ejercicio06.java
+++ Tema5/src/ejerciciosT5CopiaInsercion/Ejercicio06.java
@@ -1,5 +1,8 @@
 package ejerciciosT5CopiaInsercion;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Ejercicio06 {
 	/*
 	 * Crea un programa que cree dos arrays de enteros de tamaño 100. Luego
@@ -10,7 +13,21 @@ public class Ejercicio06 {
 	 */
 
 	public static void main(String[] args) {
-
+		int tablaOrdenada[] = new int[100]; // Array para los numeros pares
+		int tablaInversa[] = new int[100];
+		int contador = 0;
+		Scanner sc = new Scanner(System.in); // Abrimos escáner
+		for (int i = 0; i < tablaOrdenada.length; i++) { // For para ir pidiendo los números que irán al array
+			tablaOrdenada[i] = i + 1; // Vamos rellenando el array
+		}
+		for (int i = tablaOrdenada.length - 1; i >= 0; i--) { // Declaramos el bucle en 0 ya que es descendente
+			
+			tablaInversa[contador] = tablaOrdenada[i];
+			contador++; // Contador lo colocamos abajo., ya que la primera posicion es el 100
+		}
+		System.out.println("Tabla Ordenada:");
+		System.out.println(Arrays.toString(tablaOrdenada));
+		System.out.println("Tabla Desordenada:");
+		System.out.println(Arrays.toString(tablaInversa));
 	}
-
 }
--- Tema5/src/ejerciciosT5CopiaInsercion/Ejercicio07.java
+++ Tema5/src/ejerciciosT5CopiaInsercion/Ejercicio07.java
@@ -1,10 +1,27 @@
 package ejerciciosT5CopiaInsercion;
 
+import java.util.Scanner;
+
 public class Ejercicio07 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/**
+		 * Crea un programa que cree un array de 10 enteros y luego muestre el siguiente
+		 * menú con distintas opciones: 
+		 * Mostrar valores. 
+		 * Introducir valor. 
+		 * Salir. 
+		 * La opción ‘a’ mostrará todos los valores por pantalla. La opción ‘b’ pedirá un
+		 * valor V y una posición P, luego escribirá V en la posición P del array. El
+		 * menú se repetirá indefinidamente hasta que el usuario elija la opción ‘c’ que
+		 * terminará el programa.
+		 */
+		int tablaEnteros[] = new int [10];
+		Scanner sc = new Scanner(System.in); // Abrimos escáner
+		for (int i = 0; i < tablaEnteros.length; i++) { // For para ir pidiendo los números que irán al array
+			System.out.println("Introduce un número: ");
+			tablaEnteros[i] = sc.nextInt(); // Vamos rellenando el array
+		}
 	}
 
 }
