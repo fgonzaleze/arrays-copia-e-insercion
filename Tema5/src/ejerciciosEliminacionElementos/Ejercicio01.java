5f17b6c42523a52621a4ba904fa904a42297a6bc
jgonzalez <jgonzalez@106-07.info2>
2023-01-25 14:34:39 +0100

    Ejercicio 1 opcional con for

--- Tema5/src/ejerciciosEliminacionElementos/Ejercicio01.java
+++ Tema5/src/ejerciciosEliminacionElementos/Ejercicio01.java
@@ -14,13 +14,12 @@ public class Ejercicio01 {
 		 * 
 		 */
 		int tabla[] = new int[20]; // Creamos la tabla que será de un tamaño 20
-		int pares[] = new int[0];
 		int contador = 0;
 
 		Scanner sc = new Scanner(System.in); // Abrimos escáner
 		for (int i = 0; i < tabla.length; i++) { // For para ir pidiendo los números que irán al array
 			tabla[i] = (int) (Math.random() * 100 + 1); // Calculamos i en la posición que se encuentra en el for
-		}
+		} 
 		while (contador < tabla.length) {
 			if (tabla[contador] % 2 == 0) {
 				contador++;
@@ -29,7 +28,15 @@ public class Ejercicio01 {
 				tabla = Arrays.copyOf(tabla, tabla.length - 1);
 			}
 		}
-		
+		/** Forma de hacerlo con bucles for 
+		 * 
+		 * for (int i = tabla.length-1; i >= 0; i--){
+		 * if (tabla[i] % 2 != 0) {
+		 * System.arraycopy(tabla, i + 1, tabla, i, tabla.length - i -1);
+		 * tabla = Arrays.copyOf(tabla, tabla.length - 1);
+		 * }
+		 * }
+		 */
 		System.out.println(Arrays.toString(tabla));
 	}
 }
--- Tema5/src/ejerciciosEliminacionElementos/Ejercicio02.java
+++ Tema5/src/ejerciciosEliminacionElementos/Ejercicio02.java
@@ -1,5 +1,8 @@
 package ejerciciosEliminacionElementos;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Ejercicio02 {
 
 	public static void main(String[] args) {
@@ -8,6 +11,23 @@ public class Ejercicio02 {
 		 * array. Seguidamente, el programa eliminará los números no primos.
 		 * 
 		 */
+		int tabla[] = new int[10]; // Creamos la tabla que será de un tamaño 10
+		int contador = 0;
+		Scanner sc = new Scanner(System.in); // Abrimos escáner
+		for (int i = 0; i < tabla.length; i++) { // For para ir pidiendo los números que irán al array
+			System.out.println("Introduce un número: ");
+			tabla[i] = sc.nextInt(); // Vamos rellenando el array
+		}
+		while (contador < tabla.length) {
+			if (tabla[contador] % 2 == 0) {
+				contador++;
+			} else {
+				System.arraycopy(tabla, contador + 1, tabla, contador, tabla.length - contador -1);
+				tabla = Arrays.copyOf(tabla, tabla.length - 1);
+			}
+		}
+		System.out.println(Arrays.toString(tabla));
+		sc.close();
 	}
 
 }
